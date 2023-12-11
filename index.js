a6add631689dbbf5deca4072a4de8bedd2135342
manickavasagar k <140053790+Rubyy007@users.noreply.github.com>
2023-12-11 05:49:55 -0800

    Update index.js
    cors

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import router from "./router.js";
+const  allowedOrigins = ["https://regi-login.vercel.app/"]
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Check if the request origin is in the allowedOrigins array
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    // You can also configure other CORS options here if needed
+  })
+);
+
 dotenv.config();
 const app = express();
 app.use(express.json());
